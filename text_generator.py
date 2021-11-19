cef6c2cc4d5943f2e8e7beb6533e8b777a1671db
Carlos Alcala <alcala21@gmail.com>
2021-11-19 00:28:40 -0600

    Stage 3/6. Text Generator.

--- text_generator.py
+++ text_generator.py
@@ -1,6 +1,7 @@
 # Write your code here
 from nltk.tokenize import WhitespaceTokenizer
 Tokenizer = WhitespaceTokenizer()
+from collections import defaultdict
 
 
 class TextGenerator:
@@ -18,25 +19,34 @@ class TextGenerator:
     def build_bigrams(self):
         self.bigrams = [(self.tokens[i], self.tokens[i + 1]) for i in range(len(self.tokens) - 1)]
 
-    def print_bigram_stats(self):
-        print("Number of bigrams:", len(self.bigrams))
+    def summarize_bigrams(self):
+        temp = defaultdict(dict)
+        for bi in self.bigrams:
+            temp[bi[0]][bi[1]] = temp[bi[0]].get(bi[1], 0) + 1
 
-    def print_bigram(self, i):
-        print(f"Head: {self.bigrams[i][0]}    Tail: {self.bigrams[i][1]}")
+        self.bigrams = dict([(k, dict(sorted(v.items(), reverse=True, key=lambda x: x[1]))) for k, v in temp.items()])
+        temp = None
+
+    def print_tails(self, _val):
+        print(f"Head: {_val}")
+        td = self.bigrams[_val]
+        for k, v in td.items():
+            print(f"Tail: {k:10s}    Count: {v}")
 
     def get_bigram(self):
-        print()
         while True:
+            print()
             try:
                 val = input()
                 if val == "exit":
                     break
-                val = int(val)
-                self.print_bigram(val)
+                self.print_tails(val)
             except ValueError:
                 print("Type Error. Please input an integer.")
             except IndexError:
                 print("Index Error. Please input a value that is not greater than the number of all bigrams.")
+            except KeyError:
+                print("Key Error. The requested word is not in the model. Please input another word.")
 
 
 def start():
@@ -47,7 +57,7 @@ def start():
     tg = TextGenerator(text)
     tg.tokenize()
     tg.build_bigrams()
-    tg.print_bigram_stats()
+    tg.summarize_bigrams()
     tg.get_bigram()
 
 
